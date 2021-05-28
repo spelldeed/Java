e51244181994e7507053f79cc2ebc8345e3ec147
spelldeed <70479251+spelldeed@users.noreply.github.com>
2021-05-28 11:44:08 +0530

    Create timetablee.java

---
+++ TIMETABLE_MANAGEMENT/timetablee.java
@@ -0,0 +1,19 @@
+package TIMETABLE_MANAGEMENT;
+
+public class timetablee
+{
+    String a[][] = new String[6][8];
+    private String batch_name;
+
+    void setBatch_name(String s)
+    {
+        this.batch_name = s;
+    }
+    String getBatch_name()
+    {
+        return this.batch_name;
+    }
+
+
+
+}
